8d0235283d5d2412e2039f2cb31f39ee28d61059
Sreeram Padmanabhan <sreeram.com@gmail.com>
2017-10-21 17:12:00 +0100

    fixes code quality issue

--- README.md
+++ README.md
@@ -7,4 +7,5 @@ The ultimate pipeline boilerplate
 * yarn lint
 * yarn start:web:dev
 * yarn prod
+* yarn firebase
 * yarn travis
--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -6,9 +6,7 @@ const ManifestPlugin = require('webpack-manifest-plugin');
 const { entry, dist, karma_coverage_dir, prod, logs, outputFileName, resolveExtensions } = require('../variables/webpack');
 
 module.exports = {
-  entry: {
-    app: entry
-  },
+  entry: { app: entry },
   output: {
     path: path.resolve(__dirname, '.' + dist),
     filename: outputFileName,
@@ -23,17 +21,7 @@ module.exports = {
   ],
   module: {
     rules: [
-      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-      // { test: /\.less$/, use: ['style-loader', 'css-loader?modules', 'postcss-loader',], exclude: /node_modules/ },
-      // { test: /\.css$/, use: ['style-loader', 'css-loader',]},
-      // { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, loader: "url-loader?limit=10000&mimetype=application/font-woff" },
-      // { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, loader: "url-loader?limit=10000&mimetype=application/font-woff" },
-      // { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url-loader?limit=10000&mimetype=application/octet-stream" },
-      // { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file-loader" },
-      // { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url-loader?limit=10000&mimetype=image/svg+xml" },
-      // { test: /\.html$/, loader: 'raw-loader', exclude: /node_modules/ },
-      // { test: /^((?!config).)*\.js?$/, exclude: /node_modules/, loader: 'babel-loader?cacheDirectory' },
-      // { test: /\.(png|jpg|jpeg|gif|woff|woff2|ttf|eot)$/, loader: 'file-loader', exclude: /node_modules/ }
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ }
     ]
   },
   stats: {
