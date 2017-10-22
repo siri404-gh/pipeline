029994ae339bb263ae1e037a96c390b328fcf19a
Sreeram Padmanabhan <sreeram.com@gmail.com>
2017-10-22 13:32:52 +0100

    Added React Native

---
+++ index.ios.js
@@ -0,0 +1,6 @@
+import React from "react-native";
+import App from "./src/components/mobile/app/app";
+
+const { AppRegistry } = React;
+
+AppRegistry.registerComponent("pipeline", () => App);
---
+++ src/components/mobile/app/app.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <View>
+        <Text>Hello Mobile!!</Text>
+      </View>
+    );
+  }
+}
+
+export default App;
