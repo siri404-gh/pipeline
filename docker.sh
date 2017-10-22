4282c9ecdaa7b2ab0220af88540c0ad489d0dbac
Sreeram Padmanabhan <sreeram.com@gmail.com>
2017-10-22 05:00:16 +0100

    updates

--- docker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-docker login -e sreeram.com@gmail.com -u sreeram -p qpalzm123
-if [ "$TRAVIS_BRANCH" = "master" ]; then
-    TAG="latest"
-else
-    TAG="develop"
-fi
-docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG .
-docker push $TRAVIS_REPO_SLUG
