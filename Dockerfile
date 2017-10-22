35a40219cffef12b206a96d12ac600eb07c88e66
Sreeram Padmanabhan <sreeram.com@gmail.com>
2017-10-22 04:40:38 +0100

    silent npm install

--- Dockerfile
+++ Dockerfile
@@ -8,7 +8,7 @@ COPY package.json .
 # For npm@5 or later, copy package-lock.json as well
 # COPY package.json package-lock.json ./
 
-RUN npm install
+RUN npm install --silent
 
 # Bundle app source
 COPY . .
--- docker.sh
+++ docker.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
+docker login -e sreeram.com@gmail.com -u sreeram -p qpalzm123
 if [ "$TRAVIS_BRANCH" = "master" ]; then
     TAG="latest"
 else
-    TAG="$TRAVIS_BRANCH"
+    TAG="develop"
 fi
 docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG .
 docker push $TRAVIS_REPO_SLUG
